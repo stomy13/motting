23f72578e3e984973f1d887f2d76d4c7f62ac46b
Masato Tokuse <masadinti@gmail.com>
2020-03-04 02:11:34 +0900

    Create Nuxt App

---
+++ frontend/pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-text>
+          <p>
+            大事なのは自分の心に素直になることだ。
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; スティーブ・ジョブズ</small></em>
+          </div>
+        </v-card-text>
+        <!-- <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">
+            Continue
+          </v-btn>
+        </v-card-actions> -->
+      </v-card>
+      <v-card>
+        <v-card-text>
+          <p>
+            大事なのは自分の心に素直になることだ。
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; スティーブ・ジョブズ</small></em>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
