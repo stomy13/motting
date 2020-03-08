432aaa935b85bd33c4a4db1eb2539818875a3e27
Masato Tokuse <masadinti@gmail.com>
2020-03-09 02:21:53 +0900

    Allow Cors

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/ernesto-jimenez/gogen v0.0.0-20180125220232-d7d4131e6607 // indirect
 	github.com/go-chi/chi v4.0.3+incompatible
+	github.com/go-chi/cors v1.0.0
 	github.com/jinzhu/gorm v1.9.12
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/cobra v0.0.6
--- backend/motting/server/server.go
+++ backend/motting/server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/MasatoTokuse/motting/motting/api"
 	"github.com/MasatoTokuse/motting/motting/dbaccess"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 type Serve interface {
@@ -21,6 +22,16 @@ func NewServer() *server {
 
 func NewHandler(conargs *dbaccess.ConnectArgs) *chi.Mux {
 	r := chi.NewRouter()
+
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 
