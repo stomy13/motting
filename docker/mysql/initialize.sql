45a0d96eef3b8a6bd9cbcdd785b093bfd7af0240
MasatoTokuse <48572568+MasatoTokuse@users.noreply.github.com>
2021-04-05 00:30:45 +0900

    不要ファイル削除

--- docker/mysql/initialize.sql
+++
@@ -1,8 +0,0 @@
--- container内で実行(今の所)
-CREATE DATABASE motting default character set 'utf8';
-create user 'motting' identified by 'motting';
-grant all on motting.* to 'motting';
-
-CREATE DATABASE webpush default character set 'utf8';
-create user 'webpush' identified by 'webpush';
-grant all on webpush.* to 'webpush';
