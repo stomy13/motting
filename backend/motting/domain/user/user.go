621d9a5cb97877cefdb37a0c1a6869c346996d3d
hiarc <hshiro87@gmail.com>
2021-04-29 00:25:07 +0900

    refactor: #25 godocを追加した

--- backend/motting/domain/user/user.go
+++ backend/motting/domain/user/user.go
@@ -1,7 +1,9 @@
 package user
 
+// User システムの利用者。
 type User struct {
 	userId UserId
 }
 
+// UserId ユーザーID。このドメインのID
 type UserId string
--- backend/motting/domain/witticism/witticism.go
+++ backend/motting/domain/witticism/witticism.go
@@ -4,6 +4,7 @@ import (
 	"github.com/MasatoTokuse/motting/motting/domain/user"
 )
 
+// Witticism 名言。より良い人生をつくるための言葉
 type Witticism struct {
 	id         WitticismId
 	tellerName TellerName
@@ -11,7 +12,11 @@ type Witticism struct {
 	owner      Owner
 }
 
+// WitticismId 名言のID。このドメインのID
 type WitticismId string
+// TellerName 名言の発案者名。誰が言ったか
 type TellerName string
+// Sentence 名言の内容。何を言ったか
 type Sentence string
+// Owner 名言を登録したユーザーのID
 type Owner user.UserId
